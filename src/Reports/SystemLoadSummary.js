086951264e4d3c928479ae3a620e41aa18b58276
James McNeill <james.mcneill@cedarstack.io>
2020-01-23 12:50:26 -0800

    Update system zone load summary

--- src/Reports/SystemLoadSummary.js
+++ src/Reports/SystemLoadSummary.js
@@ -4,47 +4,44 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row'
 import Tab from 'react-bootstrap/Tab';
+import { PeakConditionsCard } from '../Components/PeakConditionsCard';
 import { CustomTable } from '../Components/Table';
 import { CustomPieChart } from '../Components/PieChart';
 
 const loadTypes = {
-    "latent": "Latent",
-    "related_area": "Related Area",
-    "sensible_delayed":"Sensible Delayed",
-    "sensible_instant": "Sensible Instant",
-    "sensible_return_air": "Sensible Return Air",
-    "total": "Total",
-    "total_per_area": "Total Per Area",
-    "percent_grand_total": "Percent Grand Total",
+    "sensible_instant": "Instant Sensible (W)",
+    "sensible_delayed":"Delayed Sensible (W)",
+    "latent": "Latent (W)",
+    "total": "Total (W)",
+    "percent_grand_total": "Percent of Total (%)",
 };
 
 const loadComponents = {
-    "doas_direct_to_zone": "DOAS Direct to Zone",
-    "equipment": "Equipment",
-    "exterior_floor": "Exterior Floor",
-    "exterior_wall": "Exterior Wall",
-    "fenestration_conduction": "Fenestration - Conduction",
-    "fenestration_solar": "Fenestration - Solar",
-    "grand_total": "Grand Total",
-    "ground_contact_floor": "Ground Contact Floor",
-    "ground_contact_wall": "Ground Contact Wall",
-    "hvac_equipment_loss": "HVAC Equipment Loss",
-    "infiltration": "Infiltration",
-    "interzone_ceiling": "Interzone - Ceiling",
-    "interzone_floor": "Interzone Floor",
-    "interzone_mixing": "Interzone - Mixing",
-    "interzone_wall": "Interzone - Wall",
-    "lights": "Lights",
-    "opaque_door": "Opaque Door",
-    "other_floor": "Other - Floor",
+    "roof": "Roof",
     "other_roof": "Other - Roof",
+    "interzone_ceiling": "Ceiling",
+    "fenestration_conduction": "Glass - Conduction",
+    "fenestration_solar": "Glass - Solar",
+    "opaque_door": "Door",
+    "exterior_wall": "Wall",
+    "ground_contact_wall": "Below-grade Wall",
+    "interzone_wall": "Partition",
     "other_wall": "Other - Wall",
+    "exterior_floor": "Exterior Floor",
+    "interzone_floor": "Interior Floor",
+    "ground_contact_floor": "Slab",
+    "other_floor": "Other - Floor",
+    "infiltration": "Infiltration",
     "people": "People",
+    "lights": "Lights",
+    "equipment": "Equipment",
     "power_generation_equipment": "Power Generation Equipment",
     "refrigeration": "Refrigeration",
-    "roof": "Roof",
     "water_use_equipment": "Water Use Equipment",
+    "hvac_equipment_loss": "HVAC Equipment Loss",
     "zone_ventilation": "Zone Ventilation",
+    "interzone_mixing": "Transfer Air",
+    "doas_direct_to_zone": "DOAS Direct to Zone",
 };
 
 export class SystemLoadSummary extends React.Component {
@@ -52,54 +49,54 @@ export class SystemLoadSummary extends React.Component {
         super(props);
         this.state = {
             heating_cooling_selection: "cooling_peak_load_component_table",
-            zone_selection: 0,
-            num_zones: 0,
-            zone_list: [],
+            system_selection: 0,
+            num_systems: 0,
+            system_list: [],
         };
     } 
 
     componentDidMount() {
-        this.getZoneList()
+        this.getSystemList()
     }
 
-    handleZoneSelect (eventKey) {
+    handleSystemSelect(eventKey) {
         this.setState({
-            zone_selection: eventKey
+            system_selection: eventKey,
         });
     }
 
-    handleHeatingCoolingSelect (eventKey) {
+    handleHeatingCoolingSelect(eventKey) {
         this.setState({
             heating_cooling_selection: eventKey
         });
     }
 
-    getZoneList() {
-        // Get a list of zone names, ids, and cad_object, ids
-        var zone_list = []
+    getSystemList() {
+        // Get a list of system names, ids, and cad_object, ids
+        var system_list = []
         for (var i = 0; i < this.props.data.length; i++) {
-            zone_list.push({id: i, cad_object_id: this.props.data[i].cad_object_id, name: this.props.data[i].name});
+            system_list.push({id: i, cad_object_id: this.props.data[i].cad_object_id, name: this.props.data[i].name});
         }
 
-        this.setState({zone_list: zone_list}) // Update state with list of zones
-        this.setState({num_zones: zone_list.length});  // Update state with number of zones
+        this.setState({system_list: system_list}) // Update state with list of systems
+        this.setState({num_systems: system_list.length});  // Update state with number of systems
     }
 
-    getZoneName(id) {
-        // Get the string name of the zone given an id
-        for (var i = 0; i < this.state.zone_list.length; i++) {
-            if (this.state.zone_list[i].id == id) {
-                return this.state.zone_list[i].name
+    getSystemName(id) {
+        // Get the string name of the system given an id
+        for (var i = 0; i < this.state.system_list.length; i++) {
+            if (this.state.system_list[i].id == id) {
+                return this.state.system_list[i].name
             }
         }
     }
 
     getLoadComponents() {
-        return this.props.data[this.state.zone_selection][this.state.heating_cooling_selection];
+        return this.props.data[this.state.system_selection][this.state.heating_cooling_selection];
     }
 
     formatChartData(data) {
-        //const data = jsonData['zone_loads_by_components'][this.zone_selection][this.heating_cooling_selection];
+        //const data = jsonData['system_loads_by_components'][this.system_selection][this.heating_cooling_selection];
 
         // This variable defines the load component keys assigned to each load group.  These are used to create a radial bar chart of the load components.
         const loadGroups = {
@@ -140,7 +137,7 @@ export class SystemLoadSummary extends React.Component {
                 "power_generation_equipment",
                 "refrigeration",
                 "water_use_equipment",
-                "zone_ventilation",
+                //"system_ventilation",
             ]
         }
 
@@ -150,8 +147,10 @@ export class SystemLoadSummary extends React.Component {
         Object.keys(loadGroups).map((loadGroup) => {
             var total = 0;
             // Loop again to total the loads for each load group
+            console.log(loadGroup);
+            console.log(data);
             loadGroups[loadGroup].map((loadComponent) => total += data[loadComponent]['total'])
-            newData.push({'name': loadGroup, 'total': total})
+            newData.push({'name': loadGroup, 'value': total})
         })
 
         return newData
@@ -161,18 +160,18 @@ export class SystemLoadSummary extends React.Component {
         const data = this.getLoadComponents();
 
         return (
-            <Tab.Container id="zone-loads-report" defaultActiveKey="cooling">
+            <Tab.Container id="system-loads-report" activeKey={this.state.heating_cooling_selection} defaultActiveKey="cooling_peak_load_component_table">
                 <Row>
-                        <Dropdown onSelect={this.handleZoneSelect}>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            { this.getZoneName(this.state.zone_selection) }
+                        <Dropdown onSelect={this.handleSystemSelect.bind(this)} className="App-dropdown">
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            { this.getSystemName(this.state.system_selection) }
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
-                            {this.state.zone_list.map((zone) => <Dropdown.Item eventKey={zone.id}>{zone.name}</Dropdown.Item>)}
+                            {this.state.system_list.map((system) => <Dropdown.Item eventKey={system.id}>{system.name}</Dropdown.Item>)}
                         </Dropdown.Menu>
                         </Dropdown>
-                        <Nav variant="pills" onSelect={this.handleHeatingCoolingSelect}>
+                        <Nav variant="pills" onSelect={this.handleHeatingCoolingSelect.bind(this)} className="App-buttons">
                             <Nav.Item>
                             <Nav.Link eventKey="cooling_peak_load_component_table">Cooling</Nav.Link>
                             </Nav.Item>
@@ -184,14 +183,12 @@ export class SystemLoadSummary extends React.Component {
                 <Row>
                     <Col xs={6}><CustomTable rows={loadComponents} columns={loadTypes} data={data}/></Col>
                     <Col>
+                        <Row><PeakConditionsCard/></Row>
                         <Row>
-                            <CustomPieChart  yDataKey="total"  xDataKey={loadTypes} data={this.formatChartData(data)}/>
+                            <CustomPieChart  yDataKey="value"  xDataKey={loadTypes} data={this.formatChartData(data)}/>
                         </Row>
                         <Row>
-                            <CustomPieChart  yDataKey="total"  xDataKey={loadTypes} data={this.formatChartData(data)}/>
-                        </Row>
-                        <Row>
-                            <CustomPieChart  yDataKey="total"  xDataKey={loadTypes} data={this.formatChartData(data)}/>
+                            <CustomPieChart  yDataKey="value"  xDataKey={loadTypes} data={this.formatChartData(data)}/>
                         </Row>
                     </Col>
               </Row>
