6d886ace12dea02803f4e98a5089bcab066da148
James McNeill <james.mcneill@cedarstack.io>
2020-02-07 15:05:08 -0800

    Adjust formatting of charts

--- src/Components/PieChart.js
+++ src/Components/PieChart.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Cell, Legend, Pie, PieChart } from 'recharts';
-
-//const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#DD4132', '#9E1030', '#FE840E', '#C62168'];
-//const COLORS = ["#003f5c","#374c80","#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"];  // Equidistant purple to yellow (https://learnui.design/tools/data-color-picker.html)
-//const COLORS = ["#5A374A","#5C5571","#4A768F","#329899","#4DB68D","#90CF75","#E2E063"]; //(http://tristen.ca/hcl-picker/#/hlc/7/0.99/5A374A/E2E063)
+import './PieChart.css';
 
 export class CustomPieChart extends React.Component {
     render() {
@@ -15,30 +12,30 @@ export class CustomPieChart extends React.Component {
         }
         
         return (
-            <div>
-            <p className="App-chart-title">{title}</p>
-            <PieChart width={350} height={325}>
-            <Pie
-                data={data}
-                dataKey="value"
-                cx={150}
-                cy={150}
-                innerRadius={0}
-                outerRadius={90}
-                fill="#8884d8"
-                label
-            >
-            {
-                data.map((entry, index) => (
-                    <Cell
-                    key={this.props.name + '-' + index.toString()}
-                    fill={colors[index % colors.length]}
-                    />
-                ))
-            }
-            </Pie>
-            <Legend iconSize="12" formatter={renderLegendText}/>
-            </PieChart>
+            <div className="App-chart-container">
+                <span className="App-chart-title">{title}</span>
+                <PieChart width={350} height={300}>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        cx={170}
+                        cy={125}
+                        innerRadius={0}
+                        outerRadius={90}
+                        fill="#8884d8"
+                        label
+                    >
+                    {
+                        data.map((entry, index) => (
+                            <Cell
+                            key={this.props.name + '-' + index.toString()}
+                            fill={colors[index % colors.length]}
+                            />
+                        ))
+                    }
+                    </Pie>
+                    <Legend iconSize="12" formatter={renderLegendText}/>
+                </PieChart>
             </div>
         );
     }
