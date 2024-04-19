fc5182d30f94b3f4e35b7b3b088b16d6626851e8
Eric Ringold <ericringold@gmail.com>
2024-04-19 15:59:15 -0600

    populate design psychrometrics with fake data if not found

--- measure/systems_analysis_report_generator/resources/systems_analysis_report/mappers/design_psychrometric_mapper.rb
+++ measure/systems_analysis_report_generator/resources/systems_analysis_report/mappers/design_psychrometric_mapper.rb
@@ -15,11 +15,35 @@ module SystemsAnalysisReport
         result = klass.new
 
         result.summary = @design_psychrometric_summary_mapper.(coil_sizing_detail, location)
-        result.entering_coil = Models::AirStatePoint.new(coil_sizing_detail.coil_entering_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.coil_entering_air_humidity_ratio_at_ideal_loads_peak)
-        result.leaving_coil = Models::AirStatePoint.new(coil_sizing_detail.coil_leaving_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.coil_leaving_air_humidity_ratio_at_ideal_loads_peak)
-        result.outdoor_air = Models::AirStatePoint.new(coil_sizing_detail.outdoor_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.outdoor_air_humidity_ratio_at_ideal_loads_peak)
-        result.return_air = Models::AirStatePoint.new(coil_sizing_detail.system_return_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.system_return_air_humidity_ratio_at_ideal_loads_peak)
-        result.zone = Models::AirStatePoint.new(coil_sizing_detail.zone_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.zone_air_humidity_ratio_at_ideal_loads_peak)
+        if coil_sizing_detail.coil_entering_air_drybulb_at_ideal_loads_peak.to_i == -999
+          result.entering_coil = Models::AirStatePoint.new(24, 0.0082)
+        else
+          result.entering_coil = Models::AirStatePoint.new(coil_sizing_detail.coil_entering_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.coil_entering_air_humidity_ratio_at_ideal_loads_peak)
+        end
+
+        if coil_sizing_detail.coil_leaving_air_drybulb_at_ideal_loads_peak.to_i == -999
+          result.leaving_coil = Models::AirStatePoint.new(12.8, 0.0082)
+        else
+          result.leaving_coil = Models::AirStatePoint.new(coil_sizing_detail.coil_leaving_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.coil_leaving_air_humidity_ratio_at_ideal_loads_peak)
+        end
+
+        if coil_sizing_detail.outdoor_air_drybulb_at_ideal_loads_peak.to_i == -999
+          result.outdoor_air = Models::AirStatePoint.new(28.3, 0.0059)
+        else
+          result.outdoor_air = Models::AirStatePoint.new(coil_sizing_detail.outdoor_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.outdoor_air_humidity_ratio_at_ideal_loads_peak)
+        end
+
+        if coil_sizing_detail.system_return_air_drybulb_at_ideal_loads_peak.to_i == -999
+          result.return_air = Models::AirStatePoint.new(23.9, 0.0082)
+        else
+          result.return_air = Models::AirStatePoint.new(coil_sizing_detail.system_return_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.system_return_air_humidity_ratio_at_ideal_loads_peak)
+        end
+
+        if coil_sizing_detail.zone_air_drybulb_at_ideal_loads_peak.to_i == -999
+          result.zone = Models::AirStatePoint.new(23.9, 0.0082)
+        else
+          result.zone = Models::AirStatePoint.new(coil_sizing_detail.zone_air_drybulb_at_ideal_loads_peak, coil_sizing_detail.zone_air_humidity_ratio_at_ideal_loads_peak)
+        end
 
         result
       end
