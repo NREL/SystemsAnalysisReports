c97774fe82da008a627f18722d4e389fbb3b9c4b
Eric Ringold <ericringold@gmail.com>
2024-04-19 16:00:14 -0600

    placeholder for using upstream sql

--- measure/systems_analysis_report_generator/measure.rb
+++ measure/systems_analysis_report_generator/measure.rb
@@ -29,7 +29,7 @@ class SystemsAnalysisReportGenerator < OpenStudio::Measure::ReportingMeasure
 
     return args
   end
-  
+
   # define the outputs that the measure will create
   def outputs
     outs = OpenStudio::Measure::OSOutputVector.new
@@ -38,7 +38,7 @@ class SystemsAnalysisReportGenerator < OpenStudio::Measure::ReportingMeasure
 
     return outs
   end
-  
+
   # return a vector of IdfObject's to request EnergyPlus objects needed by the run method
   # Warning: Do not change the name of this method to be snake_case. The method must be lowerCamelCase.
   def energyPlusOutputRequests(runner, user_arguments)
@@ -56,6 +56,7 @@ class SystemsAnalysisReportGenerator < OpenStudio::Measure::ReportingMeasure
     bundle_input_dir = "#{File.dirname(__FILE__)}/resources/build"
     web_config = SystemsAnalysisReport::Config.new({file_path:self.class.get_config_path(runner)})
     data = container.json_generator.generate.to_json
+    data.
 
     SystemsAnalysisReport::Strategies::WebAppWriter.(bundle_input_dir, data, web_config)
 
@@ -63,23 +64,43 @@ class SystemsAnalysisReportGenerator < OpenStudio::Measure::ReportingMeasure
     File.write('./report_data.json', data) if debug
 
     sql_file.close
-    
+
     return true
   end
 
   def self.get_model_and_sql_file(runner)
     model = runner.lastOpenStudioModel
-    sql_file = runner.lastEnergyPlusSqlFile
 
     if model.empty?
       runner.registerError('Cannot find last model.')
+    else
+      model = model.get
     end
 
-    if sql_file.empty?
-      runner.registerError('Cannot find last sql file.')
+    sql_file = nil
+    # require 'find'
+    # run_dir = runner.workflow.runDir.to_s
+    # Find.find(run_dir) do |path|
+    #   if File.directory?(path) && File.basename(path).include?('revit_analyze_all_electric_hvac_systems')
+    #     sizing_sql_path = File.join(path, 'SizingRun/run/eplusout.sql')
+    #     if File.exist?(sizing_sql_path)
+    #       puts sizing_sql_path
+    #       sql_file = OpenStudio::SqlFile.new(OpenStudio::Path.new(sizing_sql_path))
+    #     end
+    #   end
+    # end
+
+    if sql_file.nil?
+      sql_file = runner.lastEnergyPlusSqlFile
+
+      if sql_file.empty?
+        runner.registerError('Cannot find last sql file.')
+      else
+        sql_file = sql_file.get
+      end
     end
 
-    return model.get, sql_file.get
+    return model, sql_file
   end
 
   def self.get_config_path(runner)
