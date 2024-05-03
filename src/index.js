4012bfde7b9d5e00ee9c6a33224d58d175939ec3
Eric Ringold <ericringold@gmail.com>
2024-05-03 14:06:19 -0600

    package fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from './store/index'
-import App from './App';
+import App from './app';
 import json from './data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
